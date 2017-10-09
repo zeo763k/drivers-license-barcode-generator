4a2e2770c544d6b4b5ca7b1b0dc7dcfac05d0113
Kyle Decot <kyle@joinroot.com>
2017-10-09 10:52:37 -0400

    Fixes formatter tests

--- DriversLicenseBarcodeGeneratorTests/DataElementFormatterTests.swift
+++ DriversLicenseBarcodeGeneratorTests/DataElementFormatterTests.swift
@@ -2,17 +2,25 @@ import XCTest
 
 class DataElementFormatterTests: XCTestCase {
     let date = buildDate(year: 1986, month: 9, day: 14)!
-    
+
     func testFormatString() {
-        XCTAssertEqual(DataElementFormatter.formatString("KYLEDECOT", length: 5), "KYLED")
+        XCTAssertEqual(DataElementFormatter.format(string: "KYLEDECOT", length: 5), "KYLED")
     }
-    
+
     func testFormatPostalCode() {
-        XCTAssertEqual(DataElementFormatter.formatPostalCode(postalCode: "43212"), "432120000")
+        XCTAssertEqual(DataElementFormatter.format(postalCode: "43212"), "432120000")
     }
-    
+
     func testFormatDate() {
-        XCTAssertEqual(DataElementFormatter.formatDate(date: date), "09141986")
+        XCTAssertEqual(DataElementFormatter.format(date: date), "09141986")
+    }
+
+    func testFormatEyeColor() {
+        XCTAssertEqual(DataElementFormatter.format(eyeColor: .Hazel), "HAZ")
+    }
+
+    func testFormatHeight() {
+        XCTAssertEqual(DataElementFormatter.format(height: 70), "70 IN")
     }
     
     fileprivate static func buildDate(year: Int, month: Int, day: Int) -> Date! {
@@ -25,4 +33,5 @@ class DataElementFormatterTests: XCTestCase {
 
         return calendar.date(from: dateComponents as DateComponents)!
     }
+
 }
